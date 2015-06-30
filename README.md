67825df6298f094f37a238e47fb2dbb070a7904d
alain <alain@mcneel.com>
2015-06-30 11:47:47 +0200

    delete robohelp generated help to make way for doxygen.  See 'robohelp-docs' branch for backuph

--- README.md
+++
@@ -1,1 +0,0 @@
-# rhinoscriptsyntax-api-docs
--- whproj_xml.js
+++
@@ -1,1 +0,0 @@
-gXMLBuffer ="<?xml version='1.0' encoding='utf-8' ?>  <project homepage=\"title.htm\" langid=\"1033\" datapath=\"whxdata\" toc=\"whtoc.xml\" fts=\"whfts.xml\" brs=\"whbrs.xml\" >  </project>";
